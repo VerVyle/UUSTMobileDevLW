4176e6b5e6745f09eb23cee634dae4fa7fb2ff90
vervyle <www.doc.ru.dd@gmail.com>
2024-09-06 12:43:55 +0500

    Пофиксил поворот экрана

--- lab1/src/main/java/com/vervyle/lab1/AppViewModel.kt
+++ lab1/src/main/java/com/vervyle/lab1/AppViewModel.kt
@@ -19,24 +19,22 @@ class AppViewModel @Inject constructor(
 
     private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
     private val sensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+    private var timeStamp: Long = 0L
     private val eventListener: SensorEventListener = object : SensorEventListener {
         override fun onSensorChanged(event: SensorEvent?) {
-            val rotationMatrix = FloatArray(16)
-            SensorManager.getRotationMatrixFromVector(
-                rotationMatrix,
-                event?.values ?: throw RuntimeException("???")
-            )
-            val remappedRotationMatrix = FloatArray(16)
-            SensorManager.remapCoordinateSystem(
-                rotationMatrix,
-                SensorManager.AXIS_X,
-                SensorManager.AXIS_Z,
-                remappedRotationMatrix
-            )
-            val orientations = FloatArray(3)
-            SensorManager.getOrientation(remappedRotationMatrix, orientations)
-            orientations.map { Math.toDegrees(it.toDouble()) }
-            angle.value = orientations[2]
+            if (event == null) return
+            val omegaZ = event.values[2]
+            var deltaZ = 0F
+            if (timeStamp != 0L) {
+                // Вычисляем дельту времени (в секундах)
+                val deltaTime: Float = (event.timestamp - timeStamp) * 1.0f / 1000000000.0f
+
+                // Интегрируем угловую скорость для получения угла поворота
+                deltaZ += omegaZ * deltaTime
+            }
+
+            timeStamp = event.timestamp
+            angle.value += Math.toDegrees(deltaZ.toDouble()).toFloat()
             Log.d(TAG, "onSensorChanged: new angle ${angle.value}")
         }
 
